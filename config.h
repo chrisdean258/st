9097345aea961d23cd7d0dd6c71b759a45032f19
Chris Dean <chrisdean258@gmail.com>
2020-04-01 18:44:41 -0400

    .

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 #ifndef HD
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=16:antialias=true:autohint=true";
 #else
 static char *font = "Liberation Mono:pixelsize=24:antialias=true:autohint=true";
 #endif
--- config.mk
+++ config.mk
@@ -21,7 +21,8 @@ LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft \
        `$(PKG_CONFIG) --libs freetype2`
 
 # flags
-STCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600 $(shell hd-screen && echo -DHD)
+STCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
+#$(shell hd-screen && echo -DHD)
 STCFLAGS = $(INCS) $(STCPPFLAGS) $(CPPFLAGS) $(CFLAGS)
 STLDFLAGS = $(LIBS) $(LDFLAGS)
 
